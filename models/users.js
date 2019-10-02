8664c638ef330f626e07113a30f05e64fb8bfc17
PaleHazy <austinrpg@live.com>
2019-10-01 21:11:17 -0500

    messing with ejs

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Users = sequelize.define("Users", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Users;
 };
