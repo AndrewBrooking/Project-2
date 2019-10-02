dc2d6bfd4365dc5eb2359e8e257bba0ed2c5dbab
PaleHazy <austinrpg@live.com>
2019-10-01 23:27:59 -0500

    added initial sequelize createUsers @ api-routes @ '/createUser'

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,18 @@ module.exports = function(app) {
     });
   });
 
+  app.post("/createUser", (req, res) => {
+    //set user (minimal data added right now for testing)
+    createUser(req.body.username, req.body.email);
+    //creates a user when sent a post request here
+    function createUser(userName, email) {
+      db.User.create({ uName: userName, pass: "test", email: email });
+    }
+    db.User.findAll().then(users => {
+      res.send(users)
+    })
+  });
+
   // Create a new example
   app.post("/api/users", function(req, res) {
     db.User.create(req.body).then(function(dbExample) {
