5b7e4d9a18c4f4003ce79e5bb1ac859d3f9f437d
PaleHazy <austinrpg@live.com>
2019-10-05 11:12:24 -0500

    updated from master

--- misc/usersUtil (1).js
+++ misc/usersUtil (1).js
@@ -1,10 +1,10 @@
 module.exports = {
-    nameMinLength: 4,
-    nameMaxLength: 16,
-    nameRegex: /^([a-zA-Z0-9]{4,16})$/,
-    passMinLength: 8,
-    passMaxLength: 32,
-    passRegex: /^([a-z]+[A-Z]+[0-9]+[\,\.\!\@\#\$\%\^\&\*\-\_\?]+){8,32}$/,
-    emailMinLength: 8,
-    emailMaxLength: 100
+  nameMinLength: 4,
+  nameMaxLength: 16,
+  nameRegex: /^([a-zA-Z0-9]{4,16})$/,
+  passMinLength: 8,
+  passMaxLength: 32,
+  passRegex: /^([a-z]+[A-Z]+[0-9]+[\,\.\!\@\#\$\%\^\&\*\-\_\?]+){8,32}$/,
+  emailMinLength: 8,
+  emailMaxLength: 100
 };
