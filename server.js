d5ac9a3ec7d7cb8ebe7411971f49d6d5f955d794
Andrew Brooking <atbrooking.dev@gmail.com>
2019-10-08 18:45:51 -0500

    Reverted port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 const expressLayouts = require('express-ejs-layouts');
 
