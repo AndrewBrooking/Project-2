1d1f1562475aaef761e3025ed07418d0f2fa3d9a
PaleHazy <austinrpg@live.com>
2019-10-12 10:28:33 -0500

    our father

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,91 +1,82 @@
-module.exports = function (app, db) {
-  const Op = db.Sequelize.Op
+module.exports = function(app, db) {
+  const Op = db.Sequelize.Op;
   let authenticated = false;
 
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
-    console.log(req.session.userID)
+    console.log(req.session.userID);
     db.Project.findAll({
-      order: [
-        ['updatedAt', 'DESC']
-      ]
-    }).then(function (result) {
-      console.log(req.session.userID)
+      order: [["updatedAt", "DESC"]]
+    }).then(function(result) {
+      console.log(req.session.userID);
 
       if (!req.session.userID) {
-        req.session.userID = 'nothing'
+        req.session.userID = "nothing";
       }
       db.User.findOne({
         where: {
           id: req.session.userID
         },
         include: db.Following
-      }).then(function (userResult) {
+      }).then(function(userResult) {
         if (!userResult) {
-          userResult = 'nothing'
+          userResult = "nothing";
         }
 
-        
+        // res.json(
+        //   {
+        //     msg: result,
+        //     authenticated: authenticated,
+        //     loggedIn: authenticated,
+        //     userResult: userResult
+        //   }
+        // )
 
-
-      // res.json(
-      //   {
-      //     msg: result,
-      //     authenticated: authenticated,
-      //     loggedIn: authenticated,
-      //     userResult: userResult
-      //   }
-      // )
-
-
-        res.render('index', {
+        res.render("index", {
           msg: result,
           authenticated: authenticated,
           loggedIn: authenticated,
           userResult: userResult,
-         id: req.session.userID
-       })
-
-
-      })
-
+          id: req.session.userID
+        });
+      });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get('/notloggedin', function (req, res) {
+  app.get("/notloggedin", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
-    res.render('notloggedin', {
+    res.render("notloggedin", {
       loggedIn: authenticated
-    })
-  })
-
-  app.get('/create', function (req, res) {
-    let authenticated = false;
-    if (typeof req.session.userID === 'number') {
-      authenticated = true;
-    }
-    res.render('createProject', {
-      loggedIn: authenticated,
-      userID: req.session.userID
-    })
+    });
   });
 
-  app.get('/welcome', function (req, res) {
+  app.get("/create", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
-    res.render('welcome', {
+    res.render("createProject", {
+      loggedIn: authenticated,
+      userID: req.session.userID
+    });
+  });
+
+  app.get("/welcome", function(req, res) {
+    let authenticated = false;
+    if (typeof req.session.userID === "number") {
+      authenticated = true;
+    }
+    res.render("welcome", {
       loggedIn: authenticated
-    })
+    });
   });
 
   // app.post('/redirectToFund', (req,res) => {
@@ -96,49 +87,40 @@ module.exports = function (app, db) {
   //   console.log(source)
   //   res.redirect(source)
   // })
-  app.get('/success', (req,res) => {
+  app.get("/success", (req, res) => {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
-    res.render('success', {loggedIn: authenticated})
-  })
+    res.render("success", { loggedIn: authenticated });
+  });
 
-app.get('/project/:id/fund', function (req, res) {
-console.log('hello im in the get')
-  let authenticated = false;
-  if (typeof req.session.userID === 'number') {
-    authenticated = true;
-  }
-  if(authenticated){
-  db.Project.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: db.User
-  }).then(function (result) {
-
-    db.User.findAll({}).then(user => {
-      let idhack = req.session.userID - 1
-      
+  app.get("/project/:id/fund", function(req, res) {
+    console.log("hello im in the get");
+    let authenticated = false;
+    if (typeof req.session.userID === "number") {
+      authenticated = true;
+    }
+    if (authenticated) {
+      db.Project.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: db.User
+      }).then(function(result) {
         res.render("fund", {
-          userEmail: user[idhack].email,
           msg: result,
           loggedIn: authenticated // == authenticated == logic for true or false
-        })
-      })
-    
-    
+        });
+      });
+    } else {
+      res.redirect("/welcome");
+    }
   });
-}
-else{
-  res.redirect('/welcome')
-}
-});
 
-  app.get('/project/:id', function (req, res) {
+  app.get("/project/:id", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
     db.Project.findOne({
@@ -146,60 +128,54 @@ else{
         id: req.params.id
       },
       include: db.User
-    }).then(function (result) {
+    }).then(function(result) {
       res.render("project", {
         proNumber: req.params.id,
         msg: result,
         loggedIn: authenticated // == authenticated == logic for true or false
-      })
+      });
     });
   });
 
-  app.get('/following', function (req, res) {
+  app.get("/following", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
-    
-    db.Following.findAll({
-        where: {
-          UserId: req.session.userID
-        },
-        include: db.User,
-        include: db.Project,
-        order: [
-          ['updatedAt', 'DESC']
-        ]
-      })
-      // logic = select * from Users u INNER JOIN Followings f on u.id = f.UserId INNER JOIN Projects p on f.Projectid = p.id;
-      .then(function (result) {
 
+    db.Following.findAll({
+      where: {
+        UserId: req.session.userID
+      },
+      include: db.User,
+      include: db.Project,
+      order: [["updatedAt", "DESC"]]
+    })
+      // logic = select * from Users u INNER JOIN Followings f on u.id = f.UserId INNER JOIN Projects p on f.Projectid = p.id;
+      .then(function(result) {
         db.User.findOne({
           where: {
             id: req.session.userID
           }
-        })
-        .then(function(dataUser){
-
-
-
-        res.render('following', {
-          msg: result,
-          loggedIn: authenticated,
-          dataUser: dataUser
+        }).then(function(dataUser) {
+          res.render("following", {
+            msg: result,
+            loggedIn: authenticated,
+            dataUser: dataUser
+          });
         });
-        })
       });
   });
 
-  app.get("/search", function (req, res) {
+  app.get("/search", function(req, res) {
     let authenticated = false;
-    if (typeof req.session.userID === 'number') {
+    if (typeof req.session.userID === "number") {
       authenticated = true;
     }
     db.Project.findAll({
       where: {
-        [Op.or]: [{
+        [Op.or]: [
+          {
             desc: {
               [Op.like]: `%${req.query.query}%`
             }
@@ -211,35 +187,32 @@ else{
           }
         ]
       }
-    }).then((result) => {
+    }).then(result => {
       db.User.findOne({
         where: {
           id: req.session.userID
-        },
-      }).then(function (userResult) {
+        }
+      }).then(function(userResult) {
         if (!userResult) {
-          userResult = 'nothing'
+          userResult = "nothing";
         }
 
-        res.render('index', {
+        res.render("index", {
           msg: result,
           authenticated: authenticated,
           loggedIn: authenticated,
           userResult: userResult
-        })
-      })
+        });
+      });
       // res.render("index", {
       //   msg: result,
       //   loggedIn: authenticated //logic for true or false
       // });
-    })
+    });
   });
 
-
-
-  
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.send("404");
   });
 };
